d556587dcf5f086fd5f5746d8706dc38fea0b775
vahidkianfar <vahidkianfar@gmail.com>
2022-05-12 18:57:19 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Pair_Programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] players =
+            {
+                "Adrian", "Aja", "Apshan", "Carl", "Eman", "Franco", "Hayley", "Ia", "Namita", "Nolan", "Sam",
+                "Shannon", "Sherry", "Vahid", "Vijay", "Vinu"
+            };
+            Random random = new Random();
+            players = players.OrderBy(x => random.Next()).ToArray();
+            string[] pairing = new string[players.Length];
+
+            for (int i = 0; i < players.Length-1; i++)
+            {
+                pairing[i]=players[random.Next(i,players.Length)];
+                players = players.Where(x => x != pairing[i]).ToArray();
+                //Console.WriteLine(pairing[i]);
+                Console.WriteLine("Team Number {0} : {1} and {2}",i+1, pairing[i], players[i]);
+            }
+        }
+    }
+}
