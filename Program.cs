330cdca430cff26b36ca746351edc5b5abea3496
vahidkianfar <vahidkianfar@gmail.com>
2022-05-12 19:11:50 +0100

    Creat a method.

--- Program.cs
+++ Program.cs
@@ -3,8 +3,22 @@ using System.Linq;
 
 namespace Pair_Programming
 {
-    class Program
+    class Players
     {
+        private void AssignPlayersToTeams(string[] players)
+        {
+            //This function is not followed by SOLID principles.
+            
+            string[] pairing = new string[players.Length];
+            Random random = new Random();
+            players = players.OrderBy(x => random.Next()).ToArray();
+            for (int i = 0; i < players.Length-1; i++)
+            {
+                pairing[i]=players[random.Next(i,players.Length)];
+                players = players.Where(x => x != pairing[i]).ToArray();
+                Console.WriteLine("Team Number {0} : {1} and {2}",i+1, pairing[i], players[i]);
+            }
+        }
         static void Main(string[] args)
         {
             string[] players =
@@ -12,16 +26,9 @@ namespace Pair_Programming
                 "Adrian", "Aja", "Apshan", "Carl", "Eman", "Franco", "Hayley", "Ia", "Namita", "Nolan", "Sam",
                 "Shannon", "Sherry", "Vahid", "Vijay", "Vinu"
             };
-            Random random = new Random();
-            players = players.OrderBy(x => random.Next()).ToArray();
-            string[] pairing = new string[players.Length];
-
-            for (int i = 0; i < players.Length-1; i++)
-            {
-                pairing[i]=players[random.Next(i,players.Length)];
-                players = players.Where(x => x != pairing[i]).ToArray();
-                Console.WriteLine("Team Number {0} : {1} and {2}",i+1, pairing[i], players[i]);
-            }
+            
+            Players teams = new();
+            teams.AssignPlayersToTeams(players);
         }
     }
 }
